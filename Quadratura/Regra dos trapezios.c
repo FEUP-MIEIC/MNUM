2f187f7c531a123bad80c6e2d23b554b8c5605e4
Fábio Gaspar <fabiodanielreis@hotmail.com>
2017-11-29 13:59:45 +0000

    Calculo de erro

--- Quadratura/Regra dos trapezios.c
+++ Quadratura/Regra dos trapezios.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
-double f_x(double x)
+double f_x(double x) // f(x)
 {
-    return x*x;
+    return sin(x);
+}
+double f_dx2(double x) // f''(x)
+{
+    return -sin(x);
 }
 
-double primitive(double (*f)(double), double a, double b, int n)
+/*
+    @param f Funtion to integrate
+    @param g The second derivative of the function to integrate
+    @param a 
+    @param b
+    @param n Number of iterations
+    @param *e Pointer to a variable that will be assigned with the numeric error
+    @return The value of integration
+*/
+double primitive(double (*f)(double), double (*g)(double), double a, double b, int n, double *e)
 {
     double h = (a+b)/n, x = a, I = 0;
+    *e = 0; // error
     int i;
     for(i = 0; i <= n; i++)
     {
@@ -15,7 +30,9 @@ double primitive(double (*f)(double), double a, double b, int n)
             I += (*f)(x);
         else 
             I += 2 * (*f)(x);
-
+        
+        *e -= (h*h*h/12)*(*g)(x);
+        
         x += h;
     }
 
@@ -28,9 +45,11 @@ int main()
 {
     double a, b; // range (a < b)
     int n;
+    double error;
     printf("a ? "); scanf("%lf", &a);
     printf("b ? "); scanf("%lf", &b);
     printf("Number of iterations ? "); scanf("%d", &n);
 
-    printf("Result: %lf", primitive(&f_x, a, b, n));
+    printf("Result: %E\n", primitive(&f_x, &f_dx2, a, b, n, &error));
+    printf("Error: %E\n", error);    
 }
